e4fee210ffc61dda61bf6a6e3400b6f7e435c2f5
Anderson Santos <110641610+DevAndersonSantos@users.noreply.github.com>
2023-11-12 17:05:55 -0300

    Update

--- app.js
+++ app.js
@@ -1,23 +1,20 @@
+// app.js
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const routes = require('./routes');
-const port = 3000;
-const cors = require('cors');
-const setupSwagger = require('./swagger'); // Importe o arquivo swagger.js
+const controller = require('./controller/controller');
+const port = 3000
+const cors = require('cors');   // lib para permissão de rodar esse projeto de outra origem
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Configurar o middleware CORS
-app.use(cors());
+app.use(cors());   //uso da lib para funcionar
 
-app.use('/', routes);
-
-// Configurar a documentação Swagger
-setupSwagger(app);
+app.use('/', controller);
 
 app.listen(port, () => {
-  console.log(`Rodando na porta ${port}`);
+  console.log(`Rodando na porta ${port}`)
 });
--- controller/controller.js
+++ controller/controller.js
@@ -1,38 +1,47 @@
-const model = require("../models/model");
+const express = require("express");
+const router = express.Router();
+const model = require("../models/model"); // Importe o modelo de jogos
 
-// Função para listar jogos
-exports.listarJogos = (req, res) => {
-  const jogos = model.listarJogos();
+let lastJogoId = 0; // Variável para controlar o último ID utilizado para jogos
+
+router.get("/", (req, res) => {
+  const jogos = model.listarJogos(); // Use a função listarJogos do modelo
   res.json(jogos);
-};
+});
 
-// Função para adicionar um novo jogo
-exports.adicionarJogo = (req, res) => {
+router.post("/", (req, res) => {
   const { nome, plataformas, genero, anoLancamento, preco, imagem } = req.body;
-  const novoJogo = new model.Jogo(null, nome, plataformas, genero, anoLancamento, preco, imagem);
+
+  // Incrementar o ID antes de criar um novo jogo
+  lastJogoId++;
+  const novoJogo = new model.Jogo(lastJogoId, nome, plataformas, genero, anoLancamento, preco, imagem);
   model.adicionarJogo(novoJogo);
   res.status(201).json(novoJogo);
-};
+});
 
-// Função para atualizar um jogo
-exports.atualizarJogo = (req, res) => {
+
+router.put("/:id", (req, res) => {
   const id = req.params.id;
   const { nome, plataformas, genero, anoLancamento, preco, imagem } = req.body;
+
   const jogoAtualizado = model.atualizarJogo(id, nome, plataformas, genero, anoLancamento, preco, imagem);
   if (jogoAtualizado) {
-    res.json(jogoAtualizado);
+      res.json(jogoAtualizado);
   } else {
-    res.status(404).json({ error: 'Jogo não encontrado' });
+      res.status(404).json({ error: 'Jogo não encontrado' });
   }
-};
+});
 
-// Função para excluir um jogo
-exports.excluirJogo = (req, res) => {
+
+router.delete("/:id", (req, res) => {
   const id = req.params.id;
-  const jogoExcluido = model.excluirJogo(id);
+
+  const jogoExcluido = model.excluirJogo(id); // Use a função excluirJogo do modelo
   if (jogoExcluido) {
     res.json(jogoExcluido);
   } else {
     res.status(404).json({ error: 'Jogo não encontrado' });
   }
-};
+});
+
+module.exports = router;
