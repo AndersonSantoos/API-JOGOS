8b606792c5fdaa895f4e2c91eef9ccafbd8d12b7
Anderson Santos <110641610+DevAndersonSantos@users.noreply.github.com>
2023-11-12 18:48:38 -0300

    Update no swagger.

--- swagger.js
+++ swagger.js
@@ -11,15 +11,26 @@ const options = {
     },
     servers: [
       {
-        url: 'https://api-jogos.onrender.com/', // URL de solitação para testar documentação.
+        url: 'https://api-jogos.onrender.com/', // URL de solicitação.
         description: 'Servidor da aplicação',
       },
-      
-      // {
-      //   url: 'https://api-jogos.onrender.com',
-      //   description: 'Ambiente de produção',
-      // },
     ],
+    components: {
+      schemas: {
+        Jogo: {
+          type: 'object',
+          properties: {
+            id: { type: 'integer' },
+            nome: { type: 'string' },
+            plataformas: { type: 'string' },
+            genero: { type: 'string' },
+            anoLancamento: { type: 'integer' },
+            preco: { type: 'string' },
+            imagem: { type: 'string' },
+          },
+        },
+      },
+    },
   },
   apis: ['./routes.js'],
 };
